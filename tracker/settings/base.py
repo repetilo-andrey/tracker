f53bfd8f7cafb75db3b1fe419854f7e1ac90f2a4
Repetilo Andrey <raptor1053@yandex.ru>
2013-11-02 11:54:07 +0200

    configure

--- tracker/settings/base.py
+++ tracker/settings/base.py
@@ -87,7 +87,7 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     )
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'tracker.urls'
 
 TEMPLATE_DIRS = (
     root('templates'),
@@ -138,3 +138,9 @@ try:
 except:
     print 'please add local.py'
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
